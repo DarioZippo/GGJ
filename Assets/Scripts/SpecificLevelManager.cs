e041b01e12005bd5093642e0402076f7c0bce763
FeboGamedeveloper <febogamedeveloper@gmail.com>
2024-01-27 12:05:36 +0100

    add ui

--- Assets/Scripts/SpecificLevelManager.cs
+++ Assets/Scripts/SpecificLevelManager.cs
@@ -1,14 +1,30 @@
 using Pearl;
+using Pearl.Events;
 using Pearl.Input;
-using UnityEngine;
 
 namespace Game
 {
     public class SpecificLevelManager : LevelManager
     {
+        public float currentGas = 0;
+
+        public static bool GetIstance(out SpecificLevelManager result)
+        {
+            return Singleton<SpecificLevelManager>.GetIstance(out result);
+        }
+
+        public static SpecificLevelManager GetSpecificIstance()
+        {
+            Singleton<SpecificLevelManager>.GetIstance(out SpecificLevelManager result);
+            return result;
+        }
+
+
         protected override void PearlAwake()
         {
             base.PearlAwake();
+
+            currentGas = 0;
         }
 
         protected override void PearlStart()
@@ -16,8 +32,11 @@ namespace Game
             base.PearlStart();
 
             InputManager.SetSwitchMap("Gameplay", true);
+        }
 
-            Debug.Log("ciao");
+        private void Update()
+        {
+            PearlEventsManager.CallEvent("OnGas", currentGas);
         }
 
         protected override void PauseInternal()
